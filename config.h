66f01471831e0c85b8c9468709a817c910a6134f
phat_sumo <ethanjpayne1@gmail.com>
2020-07-14 23:12:22 -0600

    update dmenu commands to a more simplified format

--- config.h
+++ config.h
@@ -71,8 +71,9 @@ static const Layout layouts[] = {
 /* commands */
 /* component of dmenucmd, manipulated in spawn() */
 static char dmenumon[2] = "0"; 
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
-  "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-i", "-hp", "discord", NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
+//   "-nb", col_gray1, "-nf", col_cyan, "-sb", col_cyan, "-sf", col_gray1, "-h", "20", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *spotifytoggle[] = { "playerctl", "-p", "spotify", "play-pause", NULL };
 static const char scratchpadname[] = "scratchpad";
